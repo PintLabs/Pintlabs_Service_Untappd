ec3ebb3e3a2978b7371f7f96d85d40a6fccb443e
Jason Austin <jfaustin@gmail.com>
2011-02-24 15:32:49 -0500

    Updated to move to v3

--- Awsm/Service/Untappd.php
+++ Awsm/Service/Untappd.php
@@ -13,7 +13,7 @@ class Awsm_Service_Untappd
      * 
      * @var string
      */
-    const URI_BASE = 'http://api.untappd.com/v2';
+    const URI_BASE = 'http://api.untappd.com/v3';
     
     /**
      * Username and password hash for the request signing
@@ -53,21 +53,41 @@ class Awsm_Service_Untappd
     /**
      * Constructor
      * 
-     * @param string $username Untappd username
-     * @param string $password Untappd password
      * @param string $apiKey Untappd-provided API key
+     * @param string *optional* $username Untappd username
+     * @param string *optional* $password Untappd password
      */
-    public function __construct($username, $password, $apiKey)
+    public function __construct($apiKey, $username = '', $password = '')
     {
         $this->_apiKey = (string) $apiKey;
-        $this->_upHash = (string) $username . ':' . md5((string) $password);
+        
+        $this->setAuthenticatedUser($username, $password);
+    }
+    
+    /**
+     * Sets the authenticated user for untappd.  If username and
+     * password vars are set to empty string, will null out the
+     * password hash needed for authenticated methods.
+     * 
+     * @param string $username Untappd username
+     * @param string $password Untappd password
+     */
+    public function setAuthenticatedUser($username, $password)
+    {
+        if ($username != '' && $password != '') {
+            $this->_upHash = (string) $username . ':' . md5((string) $password);
+        } else {
+            $this->_upHash = null;
+        }
+        
+        return $this;
     }
     
     /**
      * Returns the authenticated user's friend feed
      * 
-     * @param int $since numeric ID of the latest checkin
-     * @param int $offset offset within the dataset to move to
+     * @param int *optional* $since numeric ID of the latest checkin
+     * @param int *optional* $offset offset within the dataset to move to
      */
     public function myFriendFeed($since = '', $offset = '')
     {
@@ -76,16 +96,21 @@ class Awsm_Service_Untappd
             'offset' => $offset
         );
         
-        return $this->_request('feed', $args);
+        return $this->_request('feed', $args, true);
     }
         
     /**
      * Gets a user's info
      * 
-     * @param string $username Untappd username
+     * @param string *optional* $username Untappd username
      */
     public function userInfo($username = '')
     {
+        if ($username == '' && is_null($this->_upHash)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('username parameter or Untappd authentication parameters must be set.');
+        }
+        
         $args = array(
             'user' => $username
         );
@@ -96,12 +121,17 @@ class Awsm_Service_Untappd
     /**
      * Gets a user's checkins
      * 
-     * @param string $username Untappd username
-     * @param int $since numeric ID of the latest checkin
-     * @param int $offset offset within the dataset to move to
+     * @param string *optional* $username Untappd username
+     * @param int *optional* $since numeric ID of the latest checkin
+     * @param int *optional* $offset offset within the dataset to move to
      */
     public function userFeed($username = '', $since = '', $offset = '')
     {
+        if ($username == '' && is_null($this->_upHash)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('username parameter or Untappd authentication parameters must be set.');
+        }
+                
         $args = array(
             'user'   => $username, 
             'since'  => $since, 
@@ -114,11 +144,16 @@ class Awsm_Service_Untappd
     /**
      * Gets a user's distinct beer list
      * 
-     * @param string $username Untappd username
-     * @param int $offset offset within the dataset to move to
+     * @param string *optional* $username Untappd username
+     * @param int *optional* $offset offset within the dataset to move to
      */
     public function userDistinctBeers($username = '', $offset = '')
     {
+        if ($username == '' && is_null($this->_upHash)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('username parameter or Untappd authentication parameters must be set.');
+        }
+                
         $args = array(
             'user'   => $username, 
             'offset' => $offset
@@ -130,11 +165,16 @@ class Awsm_Service_Untappd
     /**
      * Gets a list of a user's friends
      * 
-     * @param string $username Untappd username
-     * @param int $offset offset within the dataset to move to
+     * @param string *optional* $username Untappd username
+     * @param int *optional* $offset offset within the dataset to move to
      */
     public function userFriends($username = '', $offset = '')
     {
+        if ($username == '' && is_null($this->_upHash)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('username parameter or Untappd authentication parameters must be set.');
+        }
+                
         $args = array(
             'user'   => $username, 
             'offset' => $offset
@@ -146,11 +186,16 @@ class Awsm_Service_Untappd
     /**
      * Gets a user's wish list
      * 
-     * @param string $username Untappd username
-     * @param int $offset offset within the dataset to move to
+     * @param string *optional* $username Untappd username
+     * @param int *optional* $offset offset within the dataset to move to
      */
     public function userWishlist($username = '', $offset = '')
     {
+        if ($username == '' && is_null($this->_upHash)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('username parameter or Untappd authentication parameters must be set.');
+        }
+                
         $args = array(
             'user'   => $username, 
             'offset' => $offset
@@ -163,11 +208,22 @@ class Awsm_Service_Untappd
     /**
      * Gets a list of a user's badges they have won
      * 
-     * @param string $username Untappd username
-     * @param (beer|venue|special) $sort order to sort the badges in
+     * @param string *optional* $username Untappd username
+     * @param (all|beer|venue|special) *optional* $sort order to sort the badges in
      */
-    public function userBadge($username = '', $sort = '')
+    public function userBadge($username = '', $sort = 'all')
     {
+        if ($username == '' && is_null($this->_upHash)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('username parameter or Untappd authentication parameters must be set.');
+        }
+                
+        $validSorts = array('all', 'beer', 'venue', 'special');
+        if (!in_array($sort, $validSorts)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('Sort parameter must be one of the following: ' . implode(', ', $validSorts));            
+        }
+        
         $args = array(
             'user' => $username, 
             'sort' => $sort
@@ -183,6 +239,11 @@ class Awsm_Service_Untappd
      */
     public function beerInfo($beerId)
     {
+        if (empty($beerId)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('beerId parameter must be set and not empty');            
+        }
+                
         $args = array(
             'bid' => $beerId
         );
@@ -197,6 +258,11 @@ class Awsm_Service_Untappd
      */
     public function beerSearch($searchString)
     {
+        if (empty($searchString)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('searchString parameter must be set and not empty');            
+        }
+                
         $args = array(
             'q' => $searchString
         );
@@ -205,12 +271,100 @@ class Awsm_Service_Untappd
     }
     
     /**
+     * Gets all checkins for a specified beer
+     * 
+     * @param int $beerId Untappd ID of the beer to search for
+     * @param int *optional* $since numeric ID of the latest checkin
+     * @param int *optional* $offset offset within the dataset to move to
+     */
+    public function beerFeed($beerId, $since = '', $offset = '')
+    {
+        if (empty($beerId)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('beerId parameter must be set and not empty');            
+        }
+        
+        $args = array(
+            'bid'    => $beerId,
+            'since'  => $since,
+            'offset' => $offset,
+        );
+        
+        return $this->_request('beer_checkins', $args);          
+    }
+    
+    /**
+     * Gets information about a given venue
+     * 
+     * @param int $venueId Untappd ID of the venue
+     */
+    public function venueInfo($venueId)
+    {
+        if (empty($venueId)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('venueId parameter must be set and not empty');            
+        }
+                
+        $args = array(
+            'venue_id' => $venueId,
+        );
+        
+        return $this->_request('venue_info', $args);          
+    }
+
+    /**
+     * Gets all checkins at a given venue
+     * 
+     * @param int $venueId Untappd ID of the venue
+     * @param int *optional* $since numeric ID of the latest checkin
+     * @param int *optional* $offset offset within the dataset to move to
+     */
+    public function venueFeed($venueId, $since = '', $offset = '')
+    {
+        if (empty($venueId)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('venueId parameter must be set and not empty');            
+        }
+                
+        $args = array(
+            'venue_id' => $venueId,
+            'since'    => $since,
+            'offset'   => $offset,
+        );
+        
+        return $this->_request('venue_checkins', $args);          
+    }
+    
+    /**
+     * Gets all for beers of a certain brewery
+     * 
+     * @param int $breweryId Untappd ID of the brewery
+     * @param int *optional* $since numeric ID of the latest checkin
+     * @param int *optional* $offset offset within the dataset to move to
+     */
+    public function breweryFeed($breweryId, $since = '', $offset = '')
+    {
+        if (empty($breweryId)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('breweryId parameter must be set and not empty');            
+        }
+        
+        $args = array(
+            'brewery_id' => $breweryId,
+            'since'      => $since,
+            'offset'     => $offset,
+        );
+        
+        return $this->_request('brewery_checkins', $args);          
+    }
+    
+    /**
      * Gets the public feed of checkings, also known as "the pub"
      * 
-     *@ param int $since numeric ID of the latest checkin
-     * @param int $offset offset within the dataset to move to
-     * @param float $geolng longitude to filter public feed
-     * @param float $geolat latitude to filter public feed
+     *@ param int *optional* $since numeric ID of the latest checkin
+     * @param int *optional* $offset offset within the dataset to move to
+     * @param float *optional* $longitude longitude to filter public feed
+     * @param float *optional* $latitude latitude to filter public feed
      */
     public function publicFeed($since = '', $offset = '', $longitude = '', $latitude = '')
     {
@@ -225,18 +379,110 @@ class Awsm_Service_Untappd
     }
     
     /**
+     * Gets the trending list of beers based on location
+     * 
+     * @param (all|macro|micro|local) *optional* $type Type of beers to search for
+     * @param int *optional* $limit Number of results to return
+     * @param (daily|weekly|monthly) *optional* $age Age of checkins to consider
+     * @param float *optional* $latitude Numeric latitude to filter the feed
+     * @param float *optional* $longitude Numeric longitude to filter the feed
+     */
+    public function publicTrending($type = 'all', $limit = 10, $age = 'daily', $latitude = '', $longitude = '')
+    {
+        $validTypes = array('all', 'macro', 'micro', 'local');
+        if (!in_array($type, $validTypes)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('Type parameter must be one of the following: ' . implode(', ', $validTypes));
+        }
+        
+        $validAges = array('daily', 'weekly', 'monthly');
+        if (!in_array($age, $validAges)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('Age parameter must be one of the following: ' . implode(', ', $validAges));
+        }
+        
+        // Set limit to default if it is outside of the available params
+        if ($limit > 10 || $limit < 1) {
+            $limit = 10;
+        }
+        
+        $args = array(
+            'type'   => $type,
+            'limit'  => $limit,
+            'age'    => $age,
+            'geolat' => $latitude,
+            'geolng' => $longitude,
+        );
+        
+        return $this->_request('trending', $args);        
+    }
+    
+    /**
      * Gets the details of a specific checkin
      * 
      * @param int $checkinId Untappd checkin ID
      */
     public function checkinInfo($checkinId)
     {
+        if (empty($checkinId)) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('checkinId parameter must be set and not empty');            
+        }
+                
         $args = array(
             'id' => $checkinId
         );
         
         return $this->_request('details', $args);
     }
+    
+    // Waiting for validation from untappd on arguments and details for write API
+    /*
+    public function checkin()
+    {
+        $args = array(
+            
+        );
+        
+        return $this->_request('', $args);          
+    }
+    
+    public function checkinComment()
+    {
+        $args = array(
+            
+        );
+        
+        return $this->_request('', $args);          
+    }
+    
+    public function checkinRemoveComment()
+    {
+        $args = array(
+            
+        );
+        
+        return $this->_request('', $args);          
+    }
+    
+    public function checkinToast()
+    {
+        $args = array(
+            
+        );
+        
+        return $this->_request('', $args);          
+    }
+    
+    public function checkinRemoveToast()
+    {
+        $args = array(
+            
+        );
+        
+        return $this->_request('', $args);          
+    }
+    */
     
     /**
      * Sends a request using curl to the required URI
@@ -248,11 +494,16 @@ class Awsm_Service_Untappd
      * 
      * @return stdClass object
      */
-    protected function _request($method, $args, $authenticated = true)
+    protected function _request($method, $args, $requireAuth = false)
     {
         $this->_lastRequestUri = null;
         $this->_lastRawResponse = null;
         $this->_lastParsedResponse = null;
+        
+        if (is_null($this->_upHash) && $requireAuth) {
+            require_once 'Awsm/Service/Untappd/Exception.php';
+            throw new Awsm_Service_Untappd_Exception('Method requires Untappd user authentication which is not set.');
+        }
         
         // Append the API key to the args passed in the query string
         $args['key'] = $this->_apiKey;
@@ -270,7 +521,7 @@ class Awsm_Service_Untappd
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $this->_lastRequestUri);
         
-        if ($authenticated) {
+        if (!is_null($this->_upHash)) {
             curl_setopt($ch, CURLOPT_USERPWD, $this->_upHash);
         }
           
